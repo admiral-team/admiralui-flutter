732b4764842155898156ec576e10c0d0c0ac92b7
Борисов Кирилл Анатольевич <KABorisov@inno.tech>
2022-07-26 15:41:17 +0300

    code review fixes

--- lib/admiralui_flutter.dart
+++ lib/admiralui_flutter.dart
@@ -1,4 +1,4 @@
-library admiralui;
+library admiralui_flutter;
 
 /// A Calculator.
 class Calculator {
--- test/admiralui_flutter_test.dart
+++ test/admiralui_flutter_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:admiralui/admiralui.dart';
+import 'package:admiralui_flutter/admiralui_flutter.dart';
 
 void main() {
   test('adds one to input values', () {
