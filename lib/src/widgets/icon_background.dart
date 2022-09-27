1910ef56049b115f23a9cfc59265dbdc2e77e14d
timbaton <timbaton@users.noreply.github.com>
2022-09-27 16:32:22 +0300

    [75] - add icon background cell unit

---
+++ lib/src/widgets/icon_background.dart
@@ -0,0 +1,62 @@
+import 'package:admiralui_flutter/admiralui_flutter.dart';
+import 'package:admiralui_flutter/layout/layout_grid.dart';
+import 'package:flutter/material.dart';
+
+/// A graphical icon widget drawn with a glyph from a font described in
+/// an [AdmiralIconData] such as Admiral's predefined
+/// [IconData]s in [AdmiralIcons].
+///
+/// This widget assumes that the rendered icon is squared. Non-squared icons may
+/// render incorrectly.
+class IconBackground extends StatelessWidget {
+  const IconBackground(
+    this.icon, {
+    Key? key,
+    this.size,
+    this.isEnabled = true,
+    this.iconColorEnabled,
+    this.iconColorDisabled,
+    this.iconBackgroundColor,
+  }) : super(key: key);
+
+  /// The icon to display. The available icons are described in [AdmiralIcons].
+  ///
+  /// The icon can be null, in which case the widget will render as an empty
+  /// space of the specified [size].
+  final IconData icon;
+
+  /// The size of the icon in logical pixels.
+  ///
+  /// Icons occupy a square with width and height equal to size.
+  final double? size;
+
+  final bool isEnabled;
+
+  final Color? iconColorEnabled;
+  final Color? iconColorDisabled;
+  final Color? iconBackgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    final AppTheme theme = AppThemeProvider.of(context);
+    final ColorPalette colors = theme.colors;
+
+    final Color color = isEnabled
+        ? (iconColorEnabled ?? colors.elementAccent.color())
+        : (iconColorDisabled ?? colors.elementAccent.colorWithOpacity());
+
+    return Container(
+      width: LayoutGrid.halfModule * 11,
+      height: LayoutGrid.halfModule * 11,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: iconBackgroundColor ?? colors.backgroundAdditionalOne.color(),
+      ),
+      child: Icon(
+        icon,
+        size: size,
+        color: color,
+      ),
+    );
+  }
+}
