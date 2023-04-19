30c96fe78d8903b55eeb84f2b6f0e0285e85a10a
Andyatkov <andyatkov@gmail.com>
2023-04-19 15:36:32 +0300

    [244] - Badges normal

---
+++ example/lib/screens/badges/badge_cell.dart
@@ -0,0 +1,92 @@
+import 'package:admiralui_flutter/admiralui_flutter.dart';
+import 'package:admiralui_flutter/layout/layout_grid.dart';
+import 'package:flutter/material.dart';
+
+class BadgeCellWidget extends StatefulWidget {
+  const BadgeCellWidget({
+    super.key,
+    required this.badgeStyle,
+    this.title = '',
+    this.isEnabled = true,
+  });
+
+  final BadgeStyle badgeStyle;
+  final String title;
+  final bool isEnabled;
+
+  @override
+  State<BadgeCellWidget> createState() => _BadgeCellWidgetState();
+}
+
+class _BadgeCellWidgetState extends State<BadgeCellWidget> {
+  double currentValue = 1;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final AppTheme theme = AppThemeProvider.of(context);
+    final ColorPalette colors = theme.colors;
+    final FontPalette fonts = theme.fonts;
+    return Container(
+      constraints: const BoxConstraints(
+        minHeight: LayoutGrid.module * 11,
+      ),
+      padding: const EdgeInsets.symmetric(vertical: LayoutGrid.halfModule * 3),
+      width: double.infinity,
+      child: Row(
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              SizedBox(
+                width: LayoutGrid.halfModule * 11,
+                height: LayoutGrid.halfModule * 11,
+                child: ImageBackgroundCellWidget(
+                  icon: AdmiralIcons.admiral_ic_diamond_outline,
+                  isEnabled: widget.isEnabled,
+                ),
+              ),
+              SizedBox(
+                  width: LayoutGrid.halfModule * 13,
+                  height: LayoutGrid.halfModule * 11),
+              Positioned(
+                right: 0,
+                top: 0,
+                child: BadgeWidget(
+                  title: currentValue.toInt().toString(),
+                  style: widget.badgeStyle,
+                  isEnable: widget.isEnabled,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            width: LayoutGrid.quadrupleModule,
+          ),
+          TextView(
+            widget.title,
+            font: fonts.body1,
+            textColorNormal: colors.textSecondary.color(),
+            textColorDisabled: colors.textSecondary.colorWithOpacity(),
+            isEnabled: widget.isEnabled,
+          ),
+          const Spacer(),
+          InputNumber(
+              numberValue: currentValue,
+              onChangedValue: _ChangeInputControl,
+              style: InputNumberButtonStyle.normal,
+              isEnable: widget.isEnabled),
+        ],
+      ),
+    );
+  }
+
+  void _ChangeInputControl(double value) {
+    setState(() {
+      currentValue = value;
+    });
+  }
+}
---
+++ example/lib/screens/badges/normal_badges_screen.dart
@@ -0,0 +1,94 @@
+import 'package:admiralui_flutter/admiralui_flutter.dart';
+import 'package:admiralui_flutter/layout/layout_grid.dart';
+import 'package:example/screens/badges/badge_cell.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NormalBadgesScreen extends StatefulWidget {
+  const NormalBadgesScreen({
+    super.key,
+    required this.title,
+    this.appBarHidden = false,
+  });
+
+  final String title;
+  final bool appBarHidden;
+
+  @override
+  State<NormalBadgesScreen> createState() => _NormalBadgesScreenState();
+}
+
+class _NormalBadgesScreenState extends State<NormalBadgesScreen> {
+  bool isEnabled = true;
+
+  @override
+  Widget build(BuildContext context) {
+    final AppTheme theme = AppThemeProvider.of(context);
+    final ColorPalette colors = theme.colors;
+    final FontPalette fonts = theme.fonts;
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: CupertinoButton(
+          child: const Icon(Icons.arrow_back_ios),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        bottomOpacity: 0.0,
+        elevation: 0.0,
+        backgroundColor: colors.backgroundBasic.color(),
+        title: Text(
+          widget.title,
+          style: fonts.largeTitle1.toTextStyle(
+            colors.textPrimary.color(),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: LayoutGrid.doubleModule,
+        ),
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              StandardTabs(
+                <String>['Default', 'Disabled'],
+                onTap: (String value) {
+                  setState(() {
+                    isEnabled = value == 'Default';
+                  });
+                },
+              ),
+              BadgeCellWidget(
+                  title: 'Additional',
+                  badgeStyle: BadgeStyle.additional,
+                  isEnabled: isEnabled),
+              BadgeCellWidget(
+                  title: 'Natural',
+                  badgeStyle: BadgeStyle.natural,
+                  isEnabled: isEnabled),
+              BadgeCellWidget(
+                  title: 'Default',
+                  badgeStyle: BadgeStyle.normal,
+                  isEnabled: isEnabled),
+              BadgeCellWidget(
+                  title: 'Success',
+                  badgeStyle: BadgeStyle.success,
+                  isEnabled: isEnabled),
+              BadgeCellWidget(
+                  title: 'Error',
+                  badgeStyle: BadgeStyle.error,
+                  isEnabled: isEnabled),
+              BadgeCellWidget(
+                  title: 'Attention',
+                  badgeStyle: BadgeStyle.attention,
+                  isEnabled: isEnabled)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
