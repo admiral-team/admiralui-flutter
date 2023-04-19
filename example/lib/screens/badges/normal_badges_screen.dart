3c5e4322814044acd1e350b2cf8235bd33a6effe
Andyatkov <andyatkov@gmail.com>
2023-04-19 16:14:32 +0300

    Fix tests

--- example/lib/screens/badges/normal_badges_screen.dart
+++ example/lib/screens/badges/normal_badges_screen.dart
@@ -1,6 +1,6 @@
 import 'package:admiralui_flutter/admiralui_flutter.dart';
 import 'package:admiralui_flutter/layout/layout_grid.dart';
-import 'package:example/screens/badges/badge_cell.dart';
+import 'badge_cell.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
