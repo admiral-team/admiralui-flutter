7ec0191267f2c56fcf771f2857340f8b4bf48f87
Борисов Кирилл Анатольевич <KABorisov@inno.tech>
2023-04-25 19:27:46 +0300

    Fixed Calendar date picker theme text color

--- lib/src/widgets/views/calendar/horizontal/calendar_horizontal_view_scheme.dart
+++ lib/src/widgets/views/calendar/horizontal/calendar_horizontal_view_scheme.dart
@@ -1,14 +1,17 @@
 import 'package:admiralui_flutter/admiralui_flutter.dart';
+import 'package:admiralui_flutter/theme/admiral_color.dart';
 
 class CalendarHorizontalViewScheme {
   CalendarHorizontalViewScheme({
     required this.theme,
   }) {
+    datePickerTextColor = theme.colors.textPrimary;
     monthScheme = CalendarMonthViewScheme(theme: theme);
     weekScheme = CalendarWeekViewScheme(theme: theme);
   }
 
   AppTheme theme;
+  late AColor datePickerTextColor;
   late CalendarMonthViewScheme monthScheme;
   late CalendarWeekViewScheme weekScheme;
 }
