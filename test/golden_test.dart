b4397767278bac0417b45ee93b876bc243683826
Andyatkov <andyatkov@gmail.com>
2024-01-25 10:21:07 +0300

    Fix warnings

--- test/golden_test.dart
+++ test/golden_test.dart
@@ -1,9 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 
-// ignore: avoid_relative_lib_imports
-import '../example/lib/main.dart';
-
 Future<void> main() async {
   testWidgets('description', (WidgetTester tester) async {});
   testGoldens('app', (WidgetTester tester) async {
