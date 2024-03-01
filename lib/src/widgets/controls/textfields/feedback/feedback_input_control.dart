afadafc8fe76ecce3999872aff46cd70d9c6bc7e
Алмазов Иван Александрович <silvep4@gmail.com>
2024-03-01 10:59:44 +0300

    Fix feedback

--- lib/src/widgets/controls/textfields/feedback/feedback_input_control.dart
+++ lib/src/widgets/controls/textfields/feedback/feedback_input_control.dart
@@ -4,6 +4,47 @@ import 'package:flutter/material.dart';
 
 typedef RatingChangeCallback = void Function(double rating);
 
+/// A customizable widget for capturing feedback through a star rating system.
+///
+/// Constructor:
+/// ```
+/// FeedbackInputControl({
+///   required RatingChangeCallback onRatingChanged,
+///   int starCount = 5,
+///   double rating = 0.0,
+///   bool isEnabled = true,
+///   MainAxisAlignment mainAxisAlignment = MainAxisAlignment.start,
+///   FeedbackInputControlScheme? scheme,
+/// })
+/// ```
+///
+/// Parameters:
+/// - `onRatingChanged`: A callback function triggered when the user changes
+/// the rating. It provides the new rating as a double value.
+/// - `starCount`: The total number of stars in the rating system.
+/// Defaults to `5`.
+/// - `rating`: The initial rating. Defaults to `0.0`.
+/// - `isEnabled`: A boolean indicating whether the rating control
+/// is enabled or disabled. Defaults to `true`.
+/// - `mainAxisAlignment`: The alignment of the stars within the row.
+/// Defaults to `MainAxisAlignment.start`.
+/// - `scheme`: An optional `FeedbackInputControlScheme` object that
+/// defines the color scheme for the feedback input control.
+///
+/// Example usage:
+/// ```dart
+/// FeedbackInputControl(
+///   onRatingChanged: (double rating) {
+///     // Handle the rating change
+///   },
+///   starCount: 5,
+///   rating: 3.5,
+///   isEnabled: true,
+///   mainAxisAlignment: MainAxisAlignment.center,
+///   scheme: FeedbackInputControlScheme.defaultScheme(),
+/// )
+/// ```
+///
 class FeedbackInputControl extends StatefulWidget {
   /// Creates a FeedbackInputControl.
   const FeedbackInputControl({
@@ -11,6 +52,7 @@ class FeedbackInputControl extends StatefulWidget {
     this.starCount = 5,
     this.rating = 0.0,
     this.isEnabled = true,
+    this.mainAxisAlignment = MainAxisAlignment.start,
     this.scheme,
   });
 
@@ -18,6 +60,7 @@ class FeedbackInputControl extends StatefulWidget {
   final double rating;
   final bool isEnabled;
   final RatingChangeCallback onRatingChanged;
+  final MainAxisAlignment mainAxisAlignment;
   final FeedbackInputControlScheme? scheme;
 
   @override
@@ -33,49 +76,60 @@ class _FeedbackInputControlState extends State<FeedbackInputControl> {
     final AppTheme theme = AppThemeProvider.of(context);
     scheme = widget.scheme ?? FeedbackInputControlScheme(theme: theme);
 
-    return Row(
-      children: List<Widget>.generate(
-        widget.starCount,
-        (int index) => GestureDetector(
-          onTap: () {
-            setState(() {
-              widget.onRatingChanged(index + 1.0);
-              _newRating = index + 1.0;
-            });
-          },
-          onHorizontalDragUpdate: (DragUpdateDetails dragDetails) {
-            setState(() {
-              RenderObject? obj;
-              obj = context.findRenderObject();
-              if (obj == null) {
-                return;
-              }
-              if (obj is RenderBox == false) {
-                return;
-              }
-              RenderBox box;
-              box = obj as RenderBox;
-              final Offset pos = box.globalToLocal(dragDetails.globalPosition);
-              final double i = pos.dx / (LayoutGrid.halfModule * 6);
+    return IgnorePointer(
+      ignoring: !widget.isEnabled,
+      child: Row(
+        mainAxisAlignment: widget.mainAxisAlignment,
+        children: List<Widget>.generate(
+          widget.starCount,
+          (int index) => GestureDetector(
+            onTap: () {
+              setState(() {
+                widget.onRatingChanged(index + 1.0);
+                _newRating = index + 1.0;
+              });
+            },
+            onHorizontalDragUpdate: (DragUpdateDetails dragDetails) {
+              setState(() {
+                RenderObject? obj;
+                obj = context.findRenderObject();
+                if (obj == null) {
+                  return;
+                }
+                if (obj is RenderBox == false) {
+                  return;
+                }
+                RenderBox box;
+                box = obj as RenderBox;
+                final Offset pos =
+                    box.globalToLocal(dragDetails.globalPosition);
+                final double i = pos.dx / (LayoutGrid.halfModule * 6);
 
-              _newRating = i;
-              if (_newRating > widget.starCount) {
-                _newRating = widget.starCount.toDouble();
-              }
-              if (_newRating < 0) {
-                _newRating = 0.0;
-              }
-              widget.onRatingChanged(_newRating);
-            });
-          },
-          child: Icon(
-            AdmiralIcons.admiral_ic_star_solid,
-            color: index >= _newRating
-                ? scheme.defaultColor.unsafeParameter(widget.isEnabled 
-                ? ControlState.normal : ControlState.disabled,)
-                : scheme.selectedColor.unsafeParameter(widget.isEnabled 
-                ? ControlState.normal : ControlState.disabled,),
-            size: LayoutGrid.halfModule * 6,
+                _newRating = i;
+                if (_newRating > widget.starCount) {
+                  _newRating = widget.starCount.toDouble();
+                }
+                if (_newRating < 0) {
+                  _newRating = 0.0;
+                }
+                widget.onRatingChanged(_newRating);
+              });
+            },
+            child: Icon(
+              AdmiralIcons.admiral_ic_star_solid,
+              color: index >= _newRating
+                  ? scheme.defaultColor.unsafeParameter(
+                      widget.isEnabled
+                          ? ControlState.normal
+                          : ControlState.disabled,
+                    )
+                  : scheme.selectedColor.unsafeParameter(
+                      widget.isEnabled
+                          ? ControlState.normal
+                          : ControlState.disabled,
+                    ),
+              size: LayoutGrid.halfModule * 6,
+            ),
           ),
         ),
       ),
