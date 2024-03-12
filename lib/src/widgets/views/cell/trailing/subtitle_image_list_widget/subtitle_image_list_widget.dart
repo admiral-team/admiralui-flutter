f99bbd9482b4d75051e778107b344516db4c492c
Алмазов Иван Александрович <silvep4@gmail.com>
2024-03-12 10:49:38 +0300

    Add documentation

--- lib/src/widgets/views/cell/trailing/subtitle_image_list_widget/subtitle_image_list_widget.dart
+++ lib/src/widgets/views/cell/trailing/subtitle_image_list_widget/subtitle_image_list_widget.dart
@@ -2,6 +2,43 @@ import 'package:admiralui_flutter/admiralui_flutter.dart';
 import 'package:admiralui_flutter/layout/layout_grid.dart';
 import 'package:flutter/material.dart';
 
+/// A widget that displays a subtitle with an optional image icon.
+///
+/// This widget is designed for use in cells or list items, providing a simple
+/// layout with a subtitle text and an optional image icon. It supports
+/// customization of appearance and enables or disables interaction based on
+/// the provided parameters.
+///
+/// Constructor:
+/// ```
+/// SubtitleImageCellWidget({
+///   Key? key,
+///   String? subtitle,
+///   IconData? imageWidget,
+///   bool isEnabled = true,
+///   SubtitleImageCellWidgetScheme? scheme,
+/// })
+/// ```
+///
+/// Parameters:
+/// - `subtitle`: The text to display as the subtitle.
+/// Defaults to an empty string.
+/// - `imageWidget`: The icon to display alongside the subtitle.
+/// Defaults to `null`.
+/// - `isEnabled`: A boolean indicating whether the widget
+/// is enabled or disabled.
+///   Defaults to `true`.
+/// - `scheme`: An optional `SubtitleImageCellWidgetScheme` object that defines
+///   the appearance of the widget.
+///
+/// Example usage:
+/// ```dart
+/// SubtitleImageCellWidget(
+///   subtitle: 'This is a subtitle',
+///   imageWidget: Icons.star,
+///   isEnabled: true,
+/// )
+/// ```
 class SubtitleImageCellWidget extends StatelessWidget {
   const SubtitleImageCellWidget({
     super.key,
