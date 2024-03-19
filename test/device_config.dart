3e8c77dd557c7fc6d3b07710653bd01237101e30
Andyatkov <andyatkov@gmail.com>
2024-03-19 10:45:32 +0300

    [436] - UI тесты

---
+++ test/device_config.dart
@@ -0,0 +1,18 @@
+import 'dart:ui';
+
+import 'package:golden_toolkit/golden_toolkit.dart';
+
+final List<Device> devices = <Device>[
+  const Device(
+    name: 'iPhone SE',
+    size: Size(375, 667),
+  ),
+  const Device(
+    name: 'iPhone 14',
+    size: Size(589, 1278),
+  ),
+  const Device(
+    name: 'iPad',
+    size: Size(1024, 1366),
+  ),
+];
