8475831a9e6d4a6dda3cf5617cbb5685e6c77b1d
Алмазов Иван Александрович <silvep4@gmail.com>
2024-03-21 13:48:35 +0300

    Fix appbar

--- example/lib/screens/tags_screen.dart
+++ example/lib/screens/tags_screen.dart
@@ -22,6 +22,7 @@ class _TagsScreenState extends State<TagsScreen> {
     return Scaffold(
       backgroundColor: colors.backgroundBasic.color(),
       appBar: AppBar(
+        backgroundColor: colors.backgroundBasic.color(),
         leading: BackButton(
           onPressed: () => Navigator.of(context).pop(),
         ),
