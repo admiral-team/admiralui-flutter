aaf0e36f27458037438ab36f274ccf3c7e82fd31
Andyatkov <andyatkov@gmail.com>
2024-04-05 09:41:30 +0300

    [502] - UI Tests Small Badges

--- example/test/badges_screen_golden.dart
+++ example/test/badges_screen_golden.dart
@@ -86,4 +86,84 @@ void runBadgesScreen(Device device) {
       'normal_badges_dark_disabled_screen_${device.name}',
     );
   });
+
+  testGoldens('small_badges_light_app', (WidgetTester tester) async {
+    final DeviceBuilder builder = DeviceBuilder()
+      ..overrideDevicesForAllScenarios(devices: <Device>[device])
+      ..addScenario(
+        widget: const MyApp(),
+        name: 'Small Badges Light screen',
+        onCreate: (Key scenarioWidgetKey) async {
+          await searchAndGo(tester, scenarioWidgetKey, 'Badges');
+          await selectSecondaryMenu(tester, scenarioWidgetKey, 'Small');
+        },
+      );
+
+    await tester.pumpDeviceBuilder(builder);
+    await screenMatchesGolden(
+      tester,
+      'small_badges_light_screen_${device.name}',
+    );
+  });
+
+  testGoldens('small_badges_light_disabled_app', (WidgetTester tester) async {
+    final DeviceBuilder builder = DeviceBuilder()
+      ..overrideDevicesForAllScenarios(devices: <Device>[device])
+      ..addScenario(
+        widget: const MyApp(),
+        name: 'Small Badges Light Disabled screen',
+        onCreate: (Key scenarioWidgetKey) async {
+          await searchAndGo(tester, scenarioWidgetKey, 'Badges');
+          await selectSecondaryMenu(tester, scenarioWidgetKey, 'Small');
+          await setDisabledState(tester, scenarioWidgetKey);
+        },
+      );
+
+    await tester.pumpDeviceBuilder(builder);
+    await screenMatchesGolden(
+      tester,
+      'small_badges_light_disabled_screen_${device.name}',
+    );
+  });
+
+  testGoldens('small_badges_dark_app', (WidgetTester tester) async {
+    final DeviceBuilder builder = DeviceBuilder()
+      ..overrideDevicesForAllScenarios(devices: <Device>[device])
+      ..addScenario(
+        widget: const MyApp(),
+        name: 'Small Badges Dark screen',
+        onCreate: (Key scenarioWidgetKey) async {
+          await changeTheme(tester, scenarioWidgetKey);
+          await searchAndGo(tester, scenarioWidgetKey, 'Badges');
+          await selectSecondaryMenu(tester, scenarioWidgetKey, 'Small');
+        },
+      );
+
+    await tester.pumpDeviceBuilder(builder);
+    await screenMatchesGolden(
+      tester,
+      'small_badges_dark_screen_${device.name}',
+    );
+  });
+
+  testGoldens('small_badges_dark_disabled_app', (WidgetTester tester) async {
+    final DeviceBuilder builder = DeviceBuilder()
+      ..overrideDevicesForAllScenarios(devices: <Device>[device])
+      ..addScenario(
+        widget: const MyApp(),
+        name: 'Small Badges Dark Disabled screen',
+        onCreate: (Key scenarioWidgetKey) async {
+          await changeTheme(tester, scenarioWidgetKey);
+          await searchAndGo(tester, scenarioWidgetKey, 'Badges');
+          await selectSecondaryMenu(tester, scenarioWidgetKey, 'Small');
+          await setDisabledState(tester, scenarioWidgetKey);
+        },
+      );
+
+    await tester.pumpDeviceBuilder(builder);
+    await screenMatchesGolden(
+      tester,
+      'small_badges_dark_disabled_screen_${device.name}',
+    );
+  });
 }
