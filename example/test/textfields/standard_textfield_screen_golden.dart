6c7fea266667683c72002afdc54b7494743e57f3
Andyatkov <andyatkov@gmail.com>
2024-04-17 02:11:38 +0300

    [511] - Standard TextFields UI Tests

---
+++ example/test/textfields/standard_textfield_screen_golden.dart
@@ -0,0 +1,347 @@
+import 'package:admiralui_flutter/admiralui_flutter.dart';
+
+import '../../lib/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+
+// ignore: avoid_relative_lib_imports
+import '../set_steps.dart';
+
+void runStandardTextFieldsScreen(Device device) {
+  // Light Theme
+  testGoldens('standard_textfields_light_app', (WidgetTester tester) async {
+    final DeviceBuilder builder = DeviceBuilder()
+      ..overrideDevicesForAllScenarios(devices: <Device>[device])
+      ..addScenario(
+        widget: const MyApp(),
+        name: 'Standard Textfields Light screen',
+        onCreate: (Key scenarioWidgetKey) async {
+          await searchAndGo(tester, scenarioWidgetKey, 'Text fields');
+          await selectSecondaryMenu(
+              tester, scenarioWidgetKey, 'Standard'
+          );
+        },
+      );
+
+    await tester.pumpDeviceBuilder(builder);
+    await screenMatchesGolden(
+      tester,
+      'standard_textfields_light_screen_${device.name}',
+    );
+  });
+
+  testGoldens('standard_textfields_read_only_light_app', (WidgetTester tester) async {
+    final DeviceBuilder builder = DeviceBuilder()
+      ..overrideDevicesForAllScenarios(devices: <Device>[device])
+      ..addScenario(
+        widget: const MyApp(),
+        name: 'Standard Textfields Read Only Light screen',
+        onCreate: (Key scenarioWidgetKey) async {
+          await searchAndGo(tester, scenarioWidgetKey, 'Text fields');
+          await selectSecondaryMenu(
+              tester, scenarioWidgetKey, 'Standard'
+          );
+
+          await tester.pump(new Duration(milliseconds: 50));
+          final Finder finderType = find.text('Read Only');
+          await tester.tap(finderType);
+          await tester.pump(new Duration(milliseconds: 50));
+        },
+      );
+
+    await tester.pumpDeviceBuilder(builder);
+    await screenMatchesGolden(
+      tester,
+      'standard_textfields_read_only_light_screen_${device.name}',
+    );
+  });
+
+  testGoldens('standard_textfields_error_light_app', (WidgetTester tester) async {
+    final DeviceBuilder builder = DeviceBuilder()
+      ..overrideDevicesForAllScenarios(devices: <Device>[device])
+      ..addScenario(
+        widget: const MyApp(),
+        name: 'Standard Textfields Error Light screen',
+        onCreate: (Key scenarioWidgetKey) async {
+          await searchAndGo(tester, scenarioWidgetKey, 'Text fields');
+          await selectSecondaryMenu(
+              tester, scenarioWidgetKey, 'Standard'
+          );
+
+          await tester.pump(new Duration(milliseconds: 50));
+          final Finder finderType = find.text('Error');
+          await tester.tap(finderType);
+          await tester.pump(new Duration(milliseconds: 50));
+        },
+      );
+
+    await tester.pumpDeviceBuilder(builder);
+    await screenMatchesGolden(
+      tester,
+      'standard_textfields_error_light_screen_${device.name}',
+    );
+  });
+
+  testGoldens('standard_textfields_disabled_light_app', (WidgetTester tester) async {
+    final DeviceBuilder builder = DeviceBuilder()
+      ..overrideDevicesForAllScenarios(devices: <Device>[device])
+      ..addScenario(
+        widget: const MyApp(),
+        name: 'Standard Textfields Disabled Light screen',
+        onCreate: (Key scenarioWidgetKey) async {
+          await searchAndGo(tester, scenarioWidgetKey, 'Text fields');
+          await selectSecondaryMenu(
+              tester, scenarioWidgetKey, 'Standard'
+          );
+
+          await tester.pump(new Duration(milliseconds: 50));
+          final Finder finderType = find.text('Disabled');
+          await tester.tap(finderType);
+          await tester.pump(new Duration(milliseconds: 50));
+        },
+      );
+
+    await tester.pumpDeviceBuilder(builder);
+    await screenMatchesGolden(
+      tester,
+      'standard_textfields_disabled_light_screen_${device.name}',
+    );
+  });
+
+  // Dark Theme
+  testGoldens('standard_textfields_dark_app', (WidgetTester tester) async {
+    final DeviceBuilder builder = DeviceBuilder()
+      ..overrideDevicesForAllScenarios(devices: <Device>[device])
+      ..addScenario(
+        widget: const MyApp(),
+        name: 'Standard Textfields Dark screen',
+        onCreate: (Key scenarioWidgetKey) async {
+          await changeTheme(tester, scenarioWidgetKey);
+          await searchAndGo(tester, scenarioWidgetKey, 'Text fields');
+          await selectSecondaryMenu(
+              tester, scenarioWidgetKey, 'Standard'
+          );
+        },
+      );
+
+    await tester.pumpDeviceBuilder(builder);
+    await screenMatchesGolden(
+      tester,
+      'standard_textfields_dark_screen_${device.name}',
+    );
+  });
+
+  testGoldens('standard_textfields_read_only_dark_app', (WidgetTester tester) async {
+    final DeviceBuilder builder = DeviceBuilder()
+      ..overrideDevicesForAllScenarios(devices: <Device>[device])
+      ..addScenario(
+        widget: const MyApp(),
+        name: 'Standard Textfields Read Only Dark screen',
+        onCreate: (Key scenarioWidgetKey) async {
+          await changeTheme(tester, scenarioWidgetKey);
+          await searchAndGo(tester, scenarioWidgetKey, 'Text fields');
+          await selectSecondaryMenu(
+              tester, scenarioWidgetKey, 'Standard'
+          );
+
+          await tester.pump(new Duration(milliseconds: 50));
+          final Finder finderType = find.text('Read Only');
+          await tester.tap(finderType);
+          await tester.pump(new Duration(milliseconds: 50));
+        },
+      );
+
+    await tester.pumpDeviceBuilder(builder);
+    await screenMatchesGolden(
+      tester,
+      'standard_textfields_read_only_dark_screen_${device.name}',
+    );
+  });
+
+  testGoldens('standard_textfields_error_dark_app', (WidgetTester tester) async {
+    final DeviceBuilder builder = DeviceBuilder()
+      ..overrideDevicesForAllScenarios(devices: <Device>[device])
+      ..addScenario(
+        widget: const MyApp(),
+        name: 'Standard Textfields Error Dark screen',
+        onCreate: (Key scenarioWidgetKey) async {
+          await changeTheme(tester, scenarioWidgetKey);
+          await searchAndGo(tester, scenarioWidgetKey, 'Text fields');
+          await selectSecondaryMenu(
+              tester, scenarioWidgetKey, 'Standard'
+          );
+
+          await tester.pump(new Duration(milliseconds: 50));
+          final Finder finderType = find.text('Error');
+          await tester.tap(finderType);
+          await tester.pump(new Duration(milliseconds: 50));
+        },
+      );
+
+    await tester.pumpDeviceBuilder(builder);
+    await screenMatchesGolden(
+      tester,
+      'standard_textfields_error_dark_screen_${device.name}',
+    );
+  });
+
+  testGoldens('standard_textfields_disabled_dark_app', (WidgetTester tester) async {
+    final DeviceBuilder builder = DeviceBuilder()
+      ..overrideDevicesForAllScenarios(devices: <Device>[device])
+      ..addScenario(
+        widget: const MyApp(),
+        name: 'Standard Textfields Disabled Dark screen',
+        onCreate: (Key scenarioWidgetKey) async {
+          await changeTheme(tester, scenarioWidgetKey);
+          await searchAndGo(tester, scenarioWidgetKey, 'Text fields');
+          await selectSecondaryMenu(
+              tester, scenarioWidgetKey, 'Standard'
+          );
+
+          await tester.pump(new Duration(milliseconds: 50));
+          final Finder finderType = find.text('Disabled');
+          await tester.tap(finderType);
+          await tester.pump(new Duration(milliseconds: 50));
+        },
+      );
+
+    await tester.pumpDeviceBuilder(builder);
+    await screenMatchesGolden(
+      tester,
+      'standard_textfields_disabled_dark_screen_${device.name}',
+    );
+  });
+
+  // Unique Cases
+  // Setting many letters
+  testGoldens('standard_textfield_many_letters_app', (WidgetTester tester) async {
+    final DeviceBuilder builder = DeviceBuilder()
+      ..overrideDevicesForAllScenarios(devices: <Device>[device])
+      ..addScenario(
+        widget: const MyApp(),
+        name: 'Standard Textfields Many Letters',
+        onCreate: (Key scenarioWidgetKey) async {
+          await searchAndGo(tester, scenarioWidgetKey, 'Text fields');
+          await selectSecondaryMenu(
+              tester, scenarioWidgetKey, 'Standard'
+          );
+
+          await tester.pump(new Duration(milliseconds: 50));
+          final Finder finder = find.byKey(const Key('standardTextField'));
+          await tester.enterText(finder, '''Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.''');
+        },
+      );
+
+    await tester.pumpDeviceBuilder(builder);
+    await screenMatchesGolden(
+      tester,
+      'standard_textfield_many_letters_${device.name}',
+    );
+  });
+
+  testGoldens('secure_textfield_many_letters_app', (WidgetTester tester) async {
+    final DeviceBuilder builder = DeviceBuilder()
+      ..overrideDevicesForAllScenarios(devices: <Device>[device])
+      ..addScenario(
+        widget: const MyApp(),
+        name: 'Secure Textfields Many Letters',
+        onCreate: (Key scenarioWidgetKey) async {
+          await searchAndGo(tester, scenarioWidgetKey, 'Text fields');
+          await selectSecondaryMenu(
+              tester, scenarioWidgetKey, 'Standard'
+          );
+
+          await tester.pump(new Duration(milliseconds: 50));
+          final Finder finder = find.byKey(const Key('secureTextField'));
+          await tester.enterText(finder, '''Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.''');
+        },
+      );
+
+    await tester.pumpDeviceBuilder(builder);
+    await screenMatchesGolden(
+      tester,
+      'secure_textfield_many_letters_${device.name}',
+    );
+  });
+
+  testGoldens('multiline_textfield_many_letters_app', (WidgetTester tester) async {
+    final DeviceBuilder builder = DeviceBuilder()
+      ..overrideDevicesForAllScenarios(devices: <Device>[device])
+      ..addScenario(
+        widget: const MyApp(),
+        name: 'Multiline Textfields Many Letters',
+        onCreate: (Key scenarioWidgetKey) async {
+          await searchAndGo(tester, scenarioWidgetKey, 'Text fields');
+          await selectSecondaryMenu(
+              tester, scenarioWidgetKey, 'Standard'
+          );
+
+          await tester.pump(new Duration(milliseconds: 50));
+          final Finder finder = find.byKey(const Key('multilineTextField'));
+          await tester.enterText(finder, '''Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.''');
+        },
+      );
+
+    await tester.pumpDeviceBuilder(builder);
+    await screenMatchesGolden(
+      tester,
+      'multiline_textfield_many_letters_${device.name}',
+    );
+  });
+
+  testGoldens('tags_textfield_many_letters_app', (WidgetTester tester) async {
+    final DeviceBuilder builder = DeviceBuilder()
+      ..overrideDevicesForAllScenarios(devices: <Device>[device])
+      ..addScenario(
+        widget: const MyApp(),
+        name: 'Tags Textfields Many Letters',
+        onCreate: (Key scenarioWidgetKey) async {
+          await searchAndGo(tester, scenarioWidgetKey, 'Text fields');
+          await selectSecondaryMenu(
+              tester, scenarioWidgetKey, 'Standard'
+          );
+
+          await tester.pump(new Duration(milliseconds: 50));
+          final Finder finder = find.byKey(const Key('tagsTextField'));
+          await tester.enterText(finder, '''Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.''');
+        },
+      );
+
+    await tester.pumpDeviceBuilder(builder);
+    await screenMatchesGolden(
+      tester,
+      'tags_textfield_many_letters_${device.name}',
+    );
+  });
+
+  ///Secure tap eye
+  testGoldens('secure_textfield_tap_eye_app', (WidgetTester tester) async {
+    final DeviceBuilder builder = DeviceBuilder()
+      ..overrideDevicesForAllScenarios(devices: <Device>[device])
+      ..addScenario(
+        widget: const MyApp(),
+        name: 'Secure Text Fields Tap Eye',
+        onCreate: (Key scenarioWidgetKey) async {
+          await searchAndGo(tester, scenarioWidgetKey, 'Text fields');
+          await selectSecondaryMenu(
+              tester, scenarioWidgetKey, 'Standard'
+          );
+
+          await tester.pump(new Duration(milliseconds: 50));
+          final Finder finder = find.descendant(
+            of: find.byKey(scenarioWidgetKey),
+            matching: find.byIcon(AdmiralIcons.admiral_ic_eye_outline),
+          );
+          await tester.tap(finder);
+          await tester.pump(new Duration(milliseconds: 50));
+        },
+      );
+
+    await tester.pumpDeviceBuilder(builder);
+    await screenMatchesGolden(
+      tester,
+      'secure_textfield_tap_eye_${device.name}',
+    );
+  });
+}
