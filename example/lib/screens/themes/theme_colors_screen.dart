2d76fac1f13efb95aabce1ddabf87346c85bd407
Алмазов Иван Александрович <silvep4@gmail.com>
2024-04-17 20:02:53 +0300

    Add theme screens

---
+++ example/lib/screens/themes/theme_colors_screen.dart
@@ -0,0 +1,80 @@
+import 'package:admiralui_flutter/admiralui_flutter.dart';
+import 'package:admiralui_flutter/layout/layout_grid.dart';
+import 'package:admiralui_flutter/theme/admiral_color.dart';
+import 'package:flutter/material.dart';
+
+class ThemeColorsScreen extends StatelessWidget {
+  const ThemeColorsScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final AppTheme theme = AppThemeProvider.of(context);
+    final ColorPalette colors = theme.colors;
+    final FontPalette fonts = theme.fonts;
+    final AppTheme choseTheme =
+        ModalRoute.of(context)!.settings.arguments as AppTheme;
+    final Map<String, AColor> colorsPalete = choseTheme.colors.allColors();
+    return Scaffold(
+      backgroundColor: colors.backgroundBasic.color(),
+      appBar: AppBar(
+        title: Text(
+          'Colors',
+          style: fonts.subtitle2.toTextStyle(
+            colors.textPrimary.color(),
+          ),
+        ),
+        leading: BackButton(
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        bottomOpacity: 0.0,
+        elevation: 0.0,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(LayoutGrid.doubleModule),
+            child: TextView(
+              'Text',
+              font: fonts.subhead3,
+              textColorNormal: colors.elementSecondary.color(),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              addAutomaticKeepAlives: false,
+              addRepaintBoundaries: false,
+              physics: const BouncingScrollPhysics(
+                parent: AlwaysScrollableScrollPhysics(),
+              ),
+              itemCount: choseTheme.colors.allColors().length,
+              itemBuilder: (BuildContext ctx, int index) {
+                String? key = colorsPalete.keys.elementAt(index);
+                return BaseCellWidget(
+                  leadingCell: Container(
+                    width: 44,
+                    height: 44,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: colorsPalete[key]?.color()),
+                  ),
+                  centerCell: TitleSubtitleCellWidget(
+                    title: key,
+                    subtitle: colorsPalete[key]!.value.toString(),
+                  ),
+                  trailingCell: Icon(
+                    AdmiralIcons.admiral_ic_chevron_right_outline,
+                    color: colors.elementSecondary.color(),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ example/lib/screens/themes/themes_screen.dart
@@ -0,0 +1,113 @@
+import 'package:admiralui_flutter/admiralui_flutter.dart';
+import 'package:admiralui_flutter/layout/layout_grid.dart';
+import 'package:flutter/material.dart';
+import '../../navigation/tab_navigator_home.dart';
+import '../../../storage/app_theme_storage.dart';
+
+class ThemesScreen extends StatefulWidget {
+  const ThemesScreen({
+    super.key,
+    required this.onPush,
+  });
+
+  final Function(TabNavigatorRoutes route, AppTheme theme) onPush;
+
+  @override
+  State<ThemesScreen> createState() => _ThemesScreenState();
+}
+
+class _ThemesScreenState extends State<ThemesScreen> {
+  final AppThemeStorage appThemeButtonStorage = AppThemeStorage();
+
+  @override
+  void initState() {
+    super.initState();
+    appThemeButtonStorage.toggleButton();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    appThemeButtonStorage.toggleButton();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final AppTheme theme = AppThemeProvider.of(context);
+    final ColorPalette colors = theme.colors;
+    final FontPalette fonts = theme.fonts;
+
+    Widget trailingCell(AppTheme choseTheme) {
+      return Row(
+        children: <Widget>[
+          TextView(
+            theme == choseTheme ? 'Выбрано' : '',
+            font: theme.fonts.subhead4,
+            textColorNormal: theme.colors.elementAccent.color(),
+          ),
+          SizedBox(
+            width: LayoutGrid.halfModule,
+          ),
+          Icon(
+            AdmiralIcons.admiral_ic_chevron_right_outline,
+            color: colors.elementSecondary.color(),
+          ),
+        ],
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: colors.backgroundBasic.color(),
+      appBar: AppBar(
+        leading: BackButton(
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        bottomOpacity: 0.0,
+        elevation: 0.0,
+        backgroundColor: colors.backgroundBasic.color(),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(
+              LayoutGrid.doubleModule,
+            ),
+            child: TextView(
+              'Themes',
+              font: fonts.title1,
+              textColorNormal: colors.textPrimary.color(),
+            ),
+          ),
+          BaseCellWidget(
+            centerCell: TextView('Light'),
+            trailingCell: trailingCell(lightTheme),
+            onPressed: () => widget.onPush.call(
+              TabNavigatorRoutes.themeOptions,
+              lightTheme,
+            ),
+          ),
+          BaseCellWidget(
+            centerCell: TextView('Dark'),
+            trailingCell: trailingCell(darkTheme),
+            onPressed: () => widget.onPush.call(
+              TabNavigatorRoutes.themeOptions,
+              darkTheme,
+            ),
+          ),
+          Spacer(),
+          Padding(
+            padding: const EdgeInsets.all(
+              LayoutGrid.doubleModule,
+            ),
+            child: PrimaryButton(
+              title: 'Создать новую тему',
+              sizeType: ButtonSizeType.big,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
