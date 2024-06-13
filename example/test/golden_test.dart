88e74ae231277bf91466cb0eb0a0667e157f280c
Andyatkov <andyatkov@gmail.com>
2024-06-13 09:49:36 +0300

    [613] - UI Test Outline Slider tabs

--- example/test/golden_test.dart
+++ example/test/golden_test.dart
@@ -24,6 +24,7 @@ import 'radio_buttons_screen_golden.dart';
 import 'switcher_golden.dart';
 import 'tabs/informer_tabs_screen_golden.dart';
 import 'tabs/logo_tabs_screen_golden.dart';
+import 'tabs/outline_slider_tabs_screen_golden.dart';
 import 'tabs/standard_tabs_screen_golden.dart';
 import 'tags_and_chips_screen_golden.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -113,5 +114,7 @@ void main() {
     runLogoTabsScreen(device);
     // Informer Tabs
     runInformerTabsScreen(device);
+    // Outline Slider Tabs
+    runOutlineSliderTabsScreen(device);
   }
 }
