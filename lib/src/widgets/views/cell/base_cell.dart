7ee9b516c890538a591bfe8b320b672e68b88712
Алмазов Иван Александрович <silvep4@gmail.com>
2024-06-30 17:30:58 +0300

    Fix leading and trailing cells disable state

--- lib/src/widgets/views/cell/base_cell.dart
+++ lib/src/widgets/views/cell/base_cell.dart
@@ -4,16 +4,6 @@ import 'package:admiralui_flutter/theme/app_theme.dart';
 import 'package:admiralui_flutter/theme/app_theme_provider.dart';
 import 'package:flutter/material.dart';
 
-/// BaseCellWidget - a view that displays leading, trailing and image views
-///
-/// You can create a BaseCellWidget by using parameters in initializer:
-/// VoidCallback? onPressed  - The tap action of BaseCellWidget
-/// Widget? leadingCell - The leading view of BaseCellWidget.
-/// Widget? centerCell - The center view of BaseCellWidget.
-/// Widget? trailingCell- The trailing view of BaseCellWidget.
-/// double borderRadius- The border radius of BaseCellWidget.
-/// bool isEnabled - The flag that controls enabled/disabled state of BaseCellWidget.
-///
 class BaseCellWidget extends StatefulWidget {
   const BaseCellWidget({
     super.key,
@@ -44,43 +34,40 @@ class _BaseCellWidgetState extends State<BaseCellWidget> {
   late BaseCellScheme scheme;
 
   @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     final AppTheme theme = AppThemeProvider.of(context);
     scheme = widget.scheme ?? BaseCellScheme(theme: theme);
 
     final Color color =
         widget.isEnabled ? scheme.defaultColor : scheme.disabledColor;
-    return IgnorePointer(
-      ignoring: !widget.isEnabled,
-      child: Ink(
-        decoration: BoxDecoration(
-          color: color,
-          borderRadius: BorderRadius.circular(
-            widget.borderRadius,
+    return Opacity(
+      opacity: widget.isEnabled ? 1 : 0.6,
+      child: IgnorePointer(
+        ignoring: !widget.isEnabled,
+        child: Ink(
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(
+              widget.borderRadius,
+            ),
           ),
-        ),
-        child: InkWell(
-          overlayColor: MaterialStateProperty.all<Color>(Colors.transparent),
-          highlightColor: scheme.selectedColor,
-          splashColor: scheme.selectedColor,
-          onTap: () {
-            widget.onPressed?.call();
-          },
-          child: Container(
-            constraints: const BoxConstraints(
-              minHeight: LayoutGrid.module * 9,
-            ),
-            padding: const EdgeInsets.symmetric(
-              vertical: LayoutGrid.halfModule * 3,
-            ),
-            width: double.infinity,
-            child: Opacity(
-              opacity: widget.isEnabled ? 1 : 0.6,
+          child: InkWell(
+            overlayColor: MaterialStateProperty.all<Color>(Colors.transparent),
+            highlightColor: scheme.selectedColor,
+            splashColor: scheme.selectedColor,
+            onTap: () {
+              if (widget.isEnabled) {
+                widget.onPressed?.call();
+              }
+            },
+            child: Container(
+              constraints: const BoxConstraints(
+                minHeight: LayoutGrid.module * 9,
+              ),
+              padding: const EdgeInsets.symmetric(
+                vertical: LayoutGrid.halfModule * 3,
+              ),
+              width: double.infinity,
               child: Row(
                 children: <Widget>[
                   SizedBox(
