bace921108338307a5d7afd33e195381bbf434de
Алмазов Иван Александрович <silvep4@gmail.com>
2024-08-19 11:51:12 +0300

    Templates for Secondary and Ghost buttons

---
+++ example/lib/screens/ai/view_models/secondary_button_view_model.dart
@@ -0,0 +1,37 @@
+import 'package:admiralui_flutter/admiralui_flutter.dart';
+import 'package:example/screens/ai/view_models/interfaces/action_model_interface.dart';
+import 'package:example/screens/ai/view_models/interfaces/actions/action_item_model_interface.dart';
+import 'package:example/screens/ai/view_models/interfaces/id_model_interface.dart';
+import 'package:example/screens/ai/view_models/interfaces/layout_model_interface.dart';
+import 'package:flutter/widgets.dart';
+
+class SecondaryButtonViewModel
+    with LayoutModelMixin, IDModelMixin, ActionModelMixin {
+  SecondaryButtonViewModel({
+    required this.title,
+    required this.isEnabled,
+    required this.sizeType,
+    required String id,
+    List<ActionItemModelInterface>? actions,
+    IconData? iconData,
+    IconPosition? iconPosition,
+    double? width,
+    double? height,
+  }) {
+    this.id = id;
+    this.actions = actions
+        ?.where(
+            (ActionItemModelInterface action) => action.actionName == 'didTap')
+        .toList();
+    this.width = width;
+    this.height = height;
+    this.iconData = iconData;
+    this.iconPosition = iconPosition;
+  }
+
+  IconData? iconData;
+  IconPosition? iconPosition;
+  final String title;
+  final bool isEnabled;
+  final ButtonSizeType sizeType;
+}
