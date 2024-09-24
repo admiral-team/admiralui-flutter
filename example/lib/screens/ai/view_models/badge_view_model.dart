742c201445064ef7756317f6e1ce6491407b18be
Алмазов Иван Александрович <silvep4@gmail.com>
2024-09-24 14:10:04 +0300

    Some fix

--- example/lib/screens/ai/view_models/badge_view_model.dart
+++ example/lib/screens/ai/view_models/badge_view_model.dart
@@ -1,9 +1,7 @@
 import 'package:admiralui_flutter/admiralui_flutter.dart';
-import 'package:example/screens/ai/view_models/interfaces/action_model_interface.dart';
 import 'package:example/screens/ai/view_models/interfaces/id_model_interface.dart';
-import 'package:example/screens/ai/view_models/interfaces/layout_model_interface.dart';
 
-class BadgeViewModel with LayoutModelMixin, IDModelMixin, ActionModelMixin {
+class BadgeViewModel with IDModelMixin {
   BadgeViewModel({
     required String id,
     this.isEnabled = true,
